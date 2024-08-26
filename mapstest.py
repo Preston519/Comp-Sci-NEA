6690e55776789ded6aae42a98f631ab2a0b72722
Preston <preston.ip@abingdon.org.uk>
2024-08-26 09:46:38 +0800

    first testing with maps api

---
+++ mapstest.py
@@ -0,0 +1,13 @@
+import googlemaps
+import datetime
+
+gmaps = googlemaps.Client(key="AIzaSyDE2qaxHADLeBQO1zLqfDIasLOalcHWHi0")
+print(gmaps.reverse_geocode((40.714224, -73.961452))[0]["formatted_address"])
+# print(gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
+#                                                     regionCode='US',
+#                                                     locality='Mountain View', 
+#                                                     enableUspsCass=True))
+print(gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=datetime.datetime.now()))
