f70d7e1b144937b3f0bdb97e6cd6d1819c04d7d6
Preston519 <piggyip519@gmail.com>
2024-09-10 09:14:19 +0100

    final test graph maker

--- graphing.py
+++ graphing.py
@@ -12,12 +12,12 @@ class Graph:
         self.graph[node1][node2] = weight
 
 while True:
-    sortingmethod = input("Make routes based on (D)istance or (T)ime?").upper()
-    if sortingmethod == "D":
-        sortingmethod = 0
+    sortparam = input("Make routes based on (D)istance or (T)ime?").upper()
+    if sortparam == "D":
+        sortingmethod = "distance"
         break
-    elif sortingmethod == "T":
-        sortingmethod = 1
+    elif sortparam == "T":
+        sortingmethod = "duration"
         break
 gmaps = googlemaps.Client(key="AIzaSyDE2qaxHADLeBQO1zLqfDIasLOalcHWHi0")
 # gmaps = googlemaps.Client(key="AIza") # Key that doesn't work
@@ -32,8 +32,9 @@ for address in nodes:
     for address2 in nodes:
         if address == address2: continue
         try:
-            distance = gmaps.direction(address, address2, mode="driving")["bounds"]["legs"]["departure_time"]["distance"]["value"]
+            weight = gmaps.directions(address, address2, mode="driving")[0]["legs"][0][sortingmethod]["value"]
         except googlemaps.exceptions.ApiError:
             print("Invalid address input")
             sys.exit()
-        address_map.add_edge()
+        address_map.add_edge(address, address2, weight)
+print(address_map.graph)
--- mapstest.py
+++ mapstest.py
@@ -2,12 +2,13 @@ import googlemaps
 import datetime
 
 gmaps = googlemaps.Client(key="AIzaSyDE2qaxHADLeBQO1zLqfDIasLOalcHWHi0")
-print(gmaps.reverse_geocode((40.714224, -73.961452))[0]["formatted_address"])
+# print(gmaps.reverse_geocode((40.714224, -73.961452))[0]["formatted_address"])
 # print(gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
 #                                                     regionCode='US',
 #                                                     locality='Mountain View', 
 #                                                     enableUspsCass=True))
-print(gmaps.directions("Sydney Town Hall",
-                                     "aldkd",
-                                     mode="transit",
-                                     departure_time=datetime.datetime.now()))
+# print(gmaps.directions("Sydney Town Hall",
+#                                      "aldkd",
+#                                      mode="transit",
+#                                      departure_time=datetime.datetime.now()))
+print(gmaps.directions("20 Parsons Mead, Abingdon, Oxfordshire", "8 Morgan Vale, Abingdon, Oxfordshire", mode="driving")[0]["legs"])
